519e3c8b1a687c8b4eea4479dba50b7be4602e7d
NiRovil <nicolas199851@hotmail.com>
2022-04-24 01:08:38 -0300

    Ajustando commit

--- transacoes/models.py
+++ transacoes/models.py
@@ -1,9 +1,4 @@
 from django.db import models
 
 class Modelo(models.Model):
-<<<<<<< HEAD
     csv_importer = models.FileField(upload_to='csv', blank=False)   
-=======
-    csv_importer = models.FileField(upload_to='csv', blank=False)
-    
->>>>>>> 4b4f33e7db35d09e59aca8602691890e23b6aac1
--- transacoes/urls.py
+++ transacoes/urls.py
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-from webbrowser import get
-from django.urls import path
-=======
 from django.urls import include, path
->>>>>>> 4b4f33e7db35d09e59aca8602691890e23b6aac1
 from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
     path('upload', views.upload, name='upload')
-=======
-    path('upload', views.upload, name='upload'),
-    path('csv/csv/transacoes-2022-01-01.csv', views.render_csv, name='object')
->>>>>>> 4b4f33e7db35d09e59aca8602691890e23b6aac1
 ]
