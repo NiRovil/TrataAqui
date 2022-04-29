c34c79d6087032e917f2fcaeb8672a6cd0d359b8
NiRovil <nicolas199851@hotmail.com>
2022-04-29 16:33:25 -0300

    Validando se um arquivo está em branco

--- transacoes/views.py
+++ transacoes/views.py
@@ -1,8 +1,6 @@
-import os
 from django.http import HttpResponse
 from django.shortcuts import render
 import csv
-from transacoes.models import Modelo
 from transacoes.forms import CsvModelForm
 from movimento.models import ModeloMovimento
 
@@ -10,7 +8,7 @@ def upload(request):
 
     name = ''
     size = 0
-    
+
     for filename, file in request.FILES.items():
         name = request.FILES[filename].name
         size = request.FILES[filename].size
@@ -18,40 +16,29 @@ def upload(request):
     form = CsvModelForm(request.POST or None, request.FILES or None)
     if request.method == 'POST' and form.is_valid:
         form.save()
-        if render_csv(name):
-            form = CsvModelForm()
+        form = CsvModelForm()
+        with open(f'csv/csv/{name}', 'r') as file: 
+            reader = csv.reader(file, delimiter=',')
+            for linha in reader:
+                if not linha:
+                    formulario = False
+                else:
+                    ModeloMovimento.objects.create(
+                    banco_origem = linha[0],
+                    agencia_origem = linha[1],
+                    conta_origem = linha[2],
+                    banco_destino = linha[3],
+                    agencia_destino = linha[4],
+                    conta_destino = linha[5],
+                    valor_da_transacao = linha[6],
+                    data_e_hora_da_transacao = linha[7]
+                    )
+                    formulario = True
+        if formulario:
+            pass
         else:
             form = CsvModelForm()
             return HttpResponse('<h1>Arquivo em branco<h1>')
     dados = {'form':form, 'name':name, 'size':size}
     
     return render(request, 'upload.html', dados)
-
-def walk(dirname):
-    for name in os.listdir(dirname):
-        path = os.path.join(dirname, name)
-        if os.path.isfile(path):
-            return path
-        else:
-            return walk(path)
-
-def render_csv(name):
-    # name = Modelo.objects.get()
-    # name = walk('csv/csv')
-
-    with open('csv/csv/transacoes-2022-01-01.csv', 'r') as file: 
-        reader = csv.reader(file, delimiter=',')
-        for linha in reader:
-            ModeloMovimento.objects.create(
-               banco_origem = linha[0],
-               agencia_origem = linha[1],
-               conta_origem = linha[2],
-               banco_destino = linha[3],
-               agencia_destino = linha[4],
-               conta_destino = linha[5],
-               valor_da_transacao = linha[6],
-               data_e_hora_da_transacao = linha[7]
-           )
-            formulario = True
-            
-    return formulario
