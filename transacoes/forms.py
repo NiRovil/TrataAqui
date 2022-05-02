ea0c832f65b0239b1c203080f4ea1b054be88fdd
NiRovil <nicolas199851@hotmail.com>
2022-05-02 12:18:33 -0300

    Alterando funções do formulário

--- transacoes/forms.py
+++ transacoes/forms.py
@@ -1,5 +1,11 @@
+from msilib.schema import File
 from django import forms
 from .models import Modelo
+from django.core.validators import FileExtensionValidator
+
+class FormValidator(forms.Form):
+
+    arquivo = forms.FileField(validators=[FileExtensionValidator(['csv'])])
 
 class CsvModelForm(forms.ModelForm):
     class Meta:
