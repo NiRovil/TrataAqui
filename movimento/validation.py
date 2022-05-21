9ebeb76ccea4e7bf24968e4406d9a4e68746e73a
NiRovil <nicolas199851@hotmail.com>
2022-05-20 22:22:48 -0300

    Dando inicio ao processo de validação de lançamentos com informação vazia

--- movimento/validation.py
+++ movimento/validation.py
@@ -1,28 +1,34 @@
 from .models import ModeloMovimento
 from datetime import datetime
+import pandas as pd
 
 def validation(linha, validacao):
-    lista = linha.decode('utf-8')
-    lista = lista.strip()
-    lista = lista.split(',')
+    linha = linha.decode('utf-8')
+    linha = linha.strip()
+    linha = linha.split(',')
     data_inicio = None
     data_transacao = None
-    data = datetime.fromisoformat(lista[7])
+    col = 'banco_origem agencia_origem conta_origem banco_destino agencia_destino conta_destino valor_da_transacao data_e_hora_da_transacao'.split()
+    df = pd.DataFrame([linha], columns=col)
+    df = df.dropna()
+    linha_limpa = df.values.tolist()
+
+    data = datetime.fromisoformat(linha_limpa[7])
     if data_inicio is None:
-        data_inicio = datetime.fromisoformat(lista[7])
+        data_inicio = datetime.fromisoformat(linha_limpa[7])
     if data_transacao is None:
         data_transacao = ModeloMovimento.objects.dates('data_e_hora_da_transacao', 'year')
     if data_inicio.date() in data_transacao:
         validacao['index'] = 'Um arquivo com as mesmas datas e horarios já foi usado para upload!'
-  
+
     banco = ModeloMovimento(
-        banco_origem = lista[0],
-        agencia_origem = lista[1],
-        conta_origem = lista[2],
-        banco_destino = lista[3],
-        agencia_destino = lista[4],
-        conta_destino = lista[5],
-        valor_da_transacao = lista[6],
+        banco_origem = linha_limpa[0],
+        agencia_origem = linha_limpa[1],
+        conta_origem = linha_limpa[2],
+        banco_destino = linha_limpa[3],
+        agencia_destino = linha_limpa[4],
+        conta_destino = linha_limpa[5],
+        valor_da_transacao = linha_limpa[6],
         data_e_hora_da_transacao = data
     )
 
@@ -31,4 +37,3 @@ def validation(linha, validacao):
         banco.save()
     else:
         validacao['index'] = 'As datas não coincidem'
-        
