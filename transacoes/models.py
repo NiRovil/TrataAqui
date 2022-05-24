e7d6a1919480a9ba7d35c6f1bc2566ffb2cbf50e
Nicolas Robert Vilela <nicolas199851@hotmail.com>
2022-05-24 00:06:33 -0300

    Delete transacoes directory

--- transacoes/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Modelo(models.Model):
-    csv_importer = models.FileField(upload_to='csv', blank=False)   
--- transacoes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-
-urlpatterns = [
-    path('upload', views.upload, name='upload')
-]
